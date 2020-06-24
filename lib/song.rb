e1f33697a56d240f66974977ccf7835f3d09b2a6
Harley Hanna <harleyhannawrit@gmail.com>
2020-06-24 18:09:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,56 @@
+
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+  
+  def self.create
+    @song = self.new
+    @@all << @song
+    @song
+  end
+  
+  def self.new_by_name(name)
+    @song = self.new
+    @song.name = name
+    @song
+  end
+  
+  def self.create_by_name(name)
+    @song = self.new 
+    @song.name = name
+    @@all << @song
+    @song 
+  end
+  
+  def self.find_by_name(name)
+    @@all.find {|song| song.name == name}
+  end
+  
+  def self.find_or_create_by_name(song_name)
+    found_song = self.find_by_name(song_name)
+    if self.all.include?(found_song) != true 
+      self.create_by_name(song_name)
+    else
+      self.find_by_name(song_name)
+    end
+  end
+  
+  def self.alphabetical
+    sorted = @@all.sort_by{|song| song.name}
+    sorted
+  end
+  
+  def self.new_from_filename(filename)
+    song = filename.split(" - ")
+    song.delete(/[(.)]\w{2,}/)
+    @song = self.new
+    @song.name = song[1]
+    @song.artist_name = song[0]
+    @song
+  end
+  
+  
 
   def self.all
     @@all
